cbe45c144e7ef98521c489f1c53e4b30784e994d
Yale Araujo <yale850@gmail.com>
2022-12-08 09:44:39 -0300

    feat: creating input text and button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,25 @@
 import { styles } from "./styles";
 
-import { View, Text } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
-export default function Home() {
+export function Home() {
+    function handleParticipantAdd() {
+        console.log('Você clicou no botão de adicionar participante')
+    }
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
 
       <Text style={styles.eventDate}>Terça, 6 de Dezembro 2022.</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6b6b6b"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }

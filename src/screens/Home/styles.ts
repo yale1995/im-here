24a1d6b7e10113be41227aba5e1429f6fa6434b0
Yale Araujo <yale850@gmail.com>
2022-12-08 09:45:48 -0300

    style: styling button and text input

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,4 +16,24 @@ export const styles = StyleSheet.create({
     color: "#6b6b6b",
     fontSize: 16,
   },
+  input: {
+    backgroundColor: "#1f1e25",
+    height: 56,
+    borderRadius: 5,
+    color: "#fff",
+    padding: 16,
+    fontSize: 16,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31cf67",
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 24,
+  },
 });

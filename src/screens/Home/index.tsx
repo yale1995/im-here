ed74526bb625c6c1839c57fa9bc36aa9104f565a
Yale Araujo <yale850@gmail.com>
2022-12-13 09:27:40 -0300

    feat: running eslint

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,24 +2,31 @@ import { styles } from "./styles";
 
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
+import { Participant } from "../../components/Participant";
+
 export function Home() {
-    function handleParticipantAdd() {
-        console.log('Você clicou no botão de adicionar participante')
-    }
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de adicionar participante");
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
 
       <Text style={styles.eventDate}>Terça, 6 de Dezembro 2022.</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6b6b6b"
-      />
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+      <Participant />
+      <Participant />
+      <Participant />
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -17,23 +17,31 @@ export const styles = StyleSheet.create({
     fontSize: 16,
   },
   input: {
+    flex: 1,
     backgroundColor: "#1f1e25",
     height: 56,
     borderRadius: 5,
     color: "#fff",
     padding: 16,
     fontSize: 16,
+    marginRight: 12,
   },
   button: {
     width: 56,
     height: 56,
     borderRadius: 5,
     backgroundColor: "#31cf67",
-    alignItems: 'center',
-    justifyContent: 'center'
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonText: {
     color: "#fff",
     fontSize: 24,
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 36,
+    marginBottom: 42,
   },
 });
